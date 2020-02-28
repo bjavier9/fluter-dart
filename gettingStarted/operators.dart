0975411df22f807f991389bd92242f38ff71c991
Javier Balsas <javier9518@gmail.com>
2020-02-27 22:59:30 -0500

    Operators

---
+++ gettingStarted/operators.dart
@@ -0,0 +1,40 @@
+main() {
+  int aNum =12;
+  double adouble=2.35;
+  //devuelve un entero
+  var result = aNum~/adouble;
+  print(result);
+ var  result2=aNum/adouble;
+  print(result2);
+  int anum = 12;
+  anum++; //incrementa
+  print(anum);
+  ++anum;//incrementa
+   print(anum);
+  int anotherNum = anum+1;
+  print(anotherNum);
+  //operadores relacionales
+  int first = 1;
+  int second=2;
+  if(first==second)print('si igual');
+  else print('no es igual');
+
+    if(first!=second)print('si es difrente');
+  else print('no es diferente');
+String saludo='Hola';
+String saludo2='Hola';
+if(first==second||saludo==saludo2)print('si es difrenten y igual los numero');
+  else print('no es diferente');
+  if(first==second&&saludo==saludo2)print('si es difrenten y igual los numero');
+  else print('no es diferente');
+int aNUmber = 35;
+  if(!(aNUmber != 150) && aNUmber <= 150){
+ print("It's true");
+ } else print("It's false.");
+  int myNumber = 13;
+ bool isTrue = true;
+ print(myNumber is int);
+ print(myNumber is! int);
+ print(myNumber is! bool);
+ print(myNumber is bool);
+}
