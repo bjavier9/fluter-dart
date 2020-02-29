30ddcc27609915e1bc2e8809605f5bfc0434dd76
Javier Balsas <javier9518@gmail.com>
2020-02-28 22:45:36 -0500

    Operators 2

--- gettingStarted/operators.dart
+++ gettingStarted/operators.dart
@@ -37,4 +37,25 @@ int aNUmber = 35;
  print(myNumber is! int);
  print(myNumber is! bool);
  print(myNumber is bool);
+
+ int firstNum = 10;
+ int secondNum;
+ if(firstNum==10)print('the value of ${firstNum}');
+ if(secondNum==null)print('true');
+ //secondNum ??=firstNum;
+ print(secondNum); 
+//int secondNum;
+ if(firstNum == 10) print("The value of ${firstNum} is set.");
+ if (secondNum == null) print("It is true.");
+ secondNum ??= firstNum;
+ print(secondNum);
+ print("After using an assignment operator, the value changes.");
+ secondNum += secondNum;
+ print(secondNum);
+ print("After using an assignment operator, the value changes again.");
+ secondNum -= secondNum;
+ print(secondNum);
+ if (secondNum == null) print("It is true.");
+ else print("it is false, because the 'secondNUm' has thevalue of ${secondNum} now.");
+
 }
