9086c1049de0be08b2e1a9c6e3560edd7d9804c5
Javier Balsas <javier9518@gmail.com>
2020-03-05 20:49:12 -0500

    class and var

--- FunctionsAndObjects/Objects.dart
+++ FunctionsAndObjects/Objects.dart
@@ -6,7 +6,40 @@ main() {
   if(newCar.isTurnedOn()){
     print("${newCar.CarName} start, model number ${newCar.carModel}");
   }else print("${newCar.CarName} stop, model number ${newCar.carModel}");
+ int numerito = 23;
+ var oso = new bear();
+ oso.numPescado=23;
+ oso.horasDormir=12;
+ print("${oso.comerPescado(oso.numPescado)} numeros de pescados");
+   print("${oso.aumentoDePeso(oso.numPescado, oso.horasDormir)} peso del oso");
 
+ print("${oso.dormirDespuesComer(oso.horasDormir)} Horas de dormir.");
+
+}
+
+
+
+class bear{
+  int numPescado;
+  int horasDormir;
+  int aumentoPeso;
+  // int comerPescado(int numPescado){
+  //   return numPescado;
+  // }
+
+  // int dormirDespuesComer(int horasDormir){
+  //   return horasDormir;
+  // }
+  // int aumentoDePeso(int numPescado, int horasDormir ){
+  //   aumentoPeso=numPescado*horasDormir;
+  //   return aumentoPeso; 
+  // }
+
+
+  // otra forma
+  int dormirDespuesComer(int horasDormir)=>horasDormir;
+  int aumentoDePeso(int numPescado, int horasDormir )=>aumentoPeso=numPescado*horasDormir;
+  int comerPescado(int numPescado)=>numPescado;
 }
 
 class Car {
