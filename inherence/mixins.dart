2425fbe5b3b8f6eb138420130be7e4b3c3b8e712
Javier Balsas <javier9518@gmail.com>
2020-03-08 20:26:31 -0500

    Mixims

---
+++ inherence/mixins.dart
@@ -0,0 +1,47 @@
+class Animal {
+ String name = "Animal";
+ Animal(){
+ print("I am Animal class constructor.");
+ }
+ Animal.namedConstructor(){
+ print("This is parent animal named constructor.");
+ }
+ void showName(){
+ print(this.name);
+ }
+ void eat(){
+ print("Animals eat everything depending on what type it is.");
+ }
+}
+class Dog {
+ void canRun(){
+ print("I can run.");
+ }
+}
+class Cat extends Animal with Dog {//reusing another class
+ //overriding parent constructor
+ //although constructors are not inherited
+ Cat() : super(){
+ print("I am child cat class overriding super Animal class.");
+ }
+ Cat.namedCatConstructor() : super.namedConstructor(){
+ print("The child cat named constructor overrides the parent animal named constructor.");
+ }
+ @override
+ void showName(){
+ print("Hi from cat.");
+ }
+ @override
+ void eat(){
+ super.eat();
+ print("Cat doesn't eat vegetables..");
+ }
+}
+main(List<String> arguments){
+ var cat = Cat();
+ cat.name = "Meaow";
+ cat.showName();
+ cat.eat();
+  var anotherCat = Cat.namedCatConstructor();
+ anotherCat.canRun();
+}
