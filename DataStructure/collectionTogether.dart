3aebde3434019c21a10fbb41b207fe22822a2426
Javier Balsas <javier9518@gmail.com>
2020-03-19 21:49:47 -0500

    Final

--- DataStructure/collectionTogether.dart
+++ DataStructure/collectionTogether.dart
@@ -1,26 +1,36 @@
 main(List<String> args) {
-  var name;
-  var age;
+  
 
   List<Map<String, dynamic>> users =[
-{name:"Peter", age:18},
-{name:"Mira", age:20},
-{name:"jason",age:22},
- { name: "Morgan", age: 32 },
- { name: "Mary", age: 50 },
- { name: "Will", age: 86 },
- { name: "Bruce", age: 96 }
+{'name':"Peter", 'age':18},
+{'name':"Mira", 'age':20},
+{'name':"jason",'age':22},
+ { 'name': "Morgan", 'age': 32 },
+ { 'name': "Mary", 'age': 50 },
+ { 'name': "Will", 'age': 86 },
+ { 'name': "Bruce", 'age': 96 }
   ];
+//print(users);
 
-var is18AndOver = users.every((user)=>user[age]>=18);
+//users.forEach((user)=>{print(user['name'])});
+var is18AndOver = users.every((user)=>user['age']>=18);
 print(is18AndOver);
-
+users.lastIndexWhere((user)=>user['age']==96);
+var buscar =users.indexWhere((user)=>user['age']==96);
+print(buscar);
+users.removeWhere((user)=>user['age']==18);
+print(users);
 var hasNameWithLetterA = users.every((user)=>user.toString().startsWith('a'));
 print(hasNameWithLetterA);
 
-var overTwentyOne = users.where((user)=>user[age]>21);
-print(overTwentyOne.length);
+var overTwentyOne = users.any((user)=>user['name'].contains('Peter'));
+print(overTwentyOne);
 
-var underEighteen = users.singleWhere((user)=>user[age]<18, orElse: ()=>null);
+var buscando = users.where((user)=>user['name']=='Bruce').toList();
+var datos = buscando[0];
+print(datos.keys.toList());
+
+var underEighteen = users.singleWhere((user)=>user['age']<18, orElse: ()=>null);
+print(underEighteen);
 
 }
