2ef66f60b4f5427d49be4c30bc9598f226ed24d9
Javier Balsas <javier9518@gmail.com>
2020-04-01 23:33:02 -0500

    disenos app parte final

--- Flutter-1/disenos/lib/main.dart
+++ Flutter-1/disenos/lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:disenos/src/pages/basico_flutter.dart';
+import 'package:disenos/src/pages/botones_pages.dart';
 import 'package:disenos/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
  
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    // SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+    //   statusBarColor: Colors.white
+    // ));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Disenos',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico':(BuildContext context)=>BasicPage(),
-        'scroll':(BuildContext context)=>ScrollPage()
+        'scroll':(BuildContext context)=>ScrollPage(),
+        'botones':(BuildContext context)=>BotonesPage()
       },
       );
       
